60c784ec1600bdce1b613fddbf48c77b7c5aba49
hongsheng <1634340724@qq.com>
2023-05-19 05:47:46 +0800

    base工程

---
+++ video-base/src/test/java/com/video/videobase/VideoBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.videobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
