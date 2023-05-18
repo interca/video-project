b0e25926b1e8d193e1aaa72a50df717898799f42
hongsheng <1634340724@qq.com>
2023-05-19 05:15:39 +0800

    Initial commit

---
+++ src/main/java/com/video/videoplusproject/VideoPlusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.video.videoplusproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoPlusProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoPlusProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/video/videoplusproject/VideoPlusProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.videoplusproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoPlusProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
