7c782271d0ec74d374722d252f4e1de153f44594
hongsheng <1634340724@qq.com>
2023-05-22 19:46:13 +0800

    添加swagger和修正部分依赖

--- video-content/video-content-api/src/main/java/com/video/content/api/CourseBaseInfoController.java
+++ video-content/video-content-api/src/main/java/com/video/content/api/CourseBaseInfoController.java
@@ -6,18 +6,16 @@ import com.video.content.model.dto.QueryCourseParamDto;
 import com.video.content.model.po.CourseBase;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
 @RestController
 @Api(value = "课程信息编辑接口", tags = "课程信息编辑接口")
 public class CourseBaseInfoController {
-    @PostMapping("/course/list")
+    @GetMapping("/course/list")
     @ApiOperation("课程查询接口")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamDto queryCourseParams) {
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamDto queryCourseParams) {
         CourseBase courseBase = new CourseBase();
         courseBase.setId(15L);
         courseBase.setDescription("测试课程");
@@ -26,6 +24,8 @@ public class CourseBaseInfoController {
         result.setPage(1);
         result.setPageSize(10);
         result.setCounts(1);
+        System.out.println(result);
         return result;
     }
+
 }
